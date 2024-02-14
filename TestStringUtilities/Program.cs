b6a74d500552bf4bf03e54f6c844636a594a68d1
Bohdan Bohiv <bohiv.bohdan@gmail.com>
2024-02-14 21:54:45 +0100

    Task 3 done.

---
+++ TestStringUtilities/Program.cs
@@ -0,0 +1,11 @@
+﻿using Utilities;
+
+
+Console.WriteLine(StringUtilities.LongWordsCount("Marcin Jagiela"));
+Console.WriteLine(StringUtilities.LongWordsCount("The lord of the rings"));
+Console.WriteLine(StringUtilities.ToSpinalCase("Marcin Jagiela"));
+Console.WriteLine(StringUtilities.ToSpinalCase("the LorD OF thE Rings"));
+Console.WriteLine("Marcin Jagiela".ToPascalCase());
+Console.WriteLine("the LorD OF thE Rings".ToPascalCase());
+
+Console.ReadLine();
---
+++ Utilities/StringUtilities.cs
@@ -0,0 +1,12 @@
+﻿namespace Utilities
+{
+    public static class StringUtilities
+    {
+        public static int LongWordsCount(string str) =>
+            str.Split(' ').Where(word => word.Length > 2).Count();
+        public static string ToSpinalCase(string str) =>
+            string.Join('-', str.ToLower().Split(' '));
+        public static string ToPascalCase(this string str) =>
+            string.Join("", str.ToLower().Split(' ').Select(word => word[0].ToString().ToUpper() + word[1..]));
+    }
+}
