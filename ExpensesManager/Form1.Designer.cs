95880720126a01b9ca78cd60dadeba11f19bcb07
Bohdan Bohiv <bohiv.bohdan@gmail.com>
2024-02-14 23:44:37 +0100

    Task 1 part 1 done.

---
+++ ExpensesManager/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ExpensesManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "path";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(117, 37);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(464, 22);
+            this.pathTextBox.TabIndex = 1;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(30, 97);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 2;
+            this.openButton.Text = "open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(117, 97);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(464, 287);
+            this.resultTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+    }
+}
+
---
+++ ExpensesManager/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ExpensesManager
+{
+    public partial class Form1 : Form
+    {
+        private string content;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            var path = pathTextBox.Text;
+
+            try
+            {
+                content = File.ReadAllText(path);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("This file does not exist.");
+                return;
+            }
+            content = content.Replace("|", "\t");
+            resultTextBox.Text = content;
+        }
+    }
+}
