fc8d05f1521140637c5640bc0ee5d8f031a63507
Bohdan Bohiv <bohiv.bohdan@gmail.com>
2024-02-15 00:54:28 +0100

    Task 1 done.

--- ExpensesManager/Form1.cs
+++ ExpensesManager/Form1.cs
@@ -11,7 +11,18 @@ namespace ExpensesManager
         {
             InitializeComponent();
         }
-
+        private class Category
+        {
+            public int times;
+            public HashSet<string> months;
+            public double total;
+            public Category()
+            {
+                times = 0;
+                months = new HashSet<string>();
+                total = 0;
+            }
+        }
         private void openButton_Click(object sender, EventArgs e)
         {
             var path = pathTextBox.Text;
@@ -34,7 +45,7 @@ namespace ExpensesManager
 
             var lines = File.ReadAllLines(path);
 
-            var categories = new HashSet<string>();
+            var categories = new Dictionary<string, Category>();
             var total = 0.0;
             var days = new HashSet<string>();
             for (int i = 1; i < lines.Length; i++)
@@ -43,13 +54,27 @@ namespace ExpensesManager
                 var split = line.Split('|');
                 var price = double.Parse(split[1]);
                 total += price;
-                categories.Add(split[2]);
                 days.Add(split[0]);
+                if (!categories.ContainsKey(split[2]))
+                {
+                    categories[split[2]] = new Category();
+                }
+                categories[split[2]].times += 1;
+                categories[split[2]].months.Add(DateTime.Parse(split[0]).Month.ToString());
+                categories[split[2]].total += price;
             }
 
             resultTextBox.Text = $"Total expenses {total}{Environment.NewLine}" +
                 $"Number of categories {categories.Count}{Environment.NewLine}" +
-                $"Total day of payment {days.Count}";
+                $"Total day of payment {days.Count}{Environment.NewLine}" +
+                $"Categories:";
+            foreach (var category in categories)
+            {
+                resultTextBox.Text += $"{Environment.NewLine}  " +
+                    $"{category.Key}-bought {category.Value.times} times in total. " +
+                    $"Purchases in: {string.Join(", ", category.Value.months)}. " +
+                    $"Total expense: {category.Value.total}";
+            }
         }
     }
 }
